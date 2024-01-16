156ad3b30ca9f21404cf26ae873dd46ac5aad2a4
Joshua Greer <jjgreer@asu.edu>
2024-01-15 21:09:52 -0700

    implemented the Earthquake
    function that removes a random half of the generated rooms, this results in more spread

--- scenesAndScripts/Dungeon.gd
+++ scenesAndScripts/Dungeon.gd
@@ -6,6 +6,8 @@ var tile_size = 32		#size of tiles from tilemap
 var num_rooms = 50		#number rooms for initial generate (gets cut down later)
 var min_size = 4		#smallest possible length for width/height
 var max_size = 10		#largest possible length for width/height
+var horizontalSpread = 400		#biases horizontal generation over vertical 
+var theEarthquake = 0.5		#randomly destroys this percent of the rooms (providing better spread)
 
 func _ready():
 	randomize()
@@ -13,12 +15,22 @@ func _ready():
 	
 func create_rooms():
 	for i in range(num_rooms):
-		var pos = Vector2(0,0)						#the origin of the 2d plane, position is already a keyword so use pos
+		var pos = Vector2(randi_range(-horizontalSpread, horizontalSpread),0)						#the origin of the 2d plane, position is already a keyword so use pos
 		var roomInstance = Room.instantiate()			#a room...
 		var width = min_size + randi() % (max_size - min_size)		#random size between min and max
 		var height = min_size + randi() % (max_size - min_size)		#random size between min and max
 		roomInstance.room_create(pos, Vector2(width,height)* tile_size)	#room create
 		$Rooms.add_child(roomInstance)		#keep track of rooms in a list
+	
+#wait for engine to finish spreading rooms out
+	await get_tree().create_timer(1).timeout
+# remove rooms (possibly)
+	for room in $Rooms.get_children():
+		if randf() < theEarthquake:
+			room.queue_free()		#remove the room
+		else:
+			room.freeze = true		#hold the room still
+
 
 func _draw():			#strictly for visualizing
 	for room in $Rooms.get_children():
--- scenesAndScripts/Room.gd
+++ scenesAndScripts/Room.gd
@@ -6,5 +6,6 @@ func room_create(_pos, _size):
 	position = _pos
 	size = _size
 	var s = RectangleShape2D.new()	#looks like a rectangle
+	s.custom_solver_bias = 0.75
 	s.extents = size
 	$CollisionShape2D.shape = s		#hitbox like a rectangle
