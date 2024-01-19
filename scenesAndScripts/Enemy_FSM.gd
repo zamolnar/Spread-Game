9a610c95d721c9ffb062b7732e9659ba0b3b86b9
yaboiteedoh <cook.brandon624@gmail.com>
2024-01-19 13:31:27 -0500

    Created Enemy FSM
    If you got any questions feel free to reach out, this is really quick and dirty and I have no idea how to spawn in stuff or despawn but the framework is there for y'all to build off of.
    
    Just for each state, create a class that extends EnemyState and adjust the default functions and parameters to meet your needs
    
    Make sure to update the accessors within the FSM if you add stuff to specific states (it's just cleaner that way)

---
+++ scenesAndScripts/Enemy_FSM.gd
@@ -0,0 +1,44 @@
+extends Node
+
+class_name EnemyFSM
+
+@export var current_state : EnemyState
+@export var enemy : CharacterBody2D
+@export var animation_tree : AnimationTree
+
+var states : Array[EnemyState]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child in get_children():
+		if(child is EnemyState):
+			states.append(child)
+			
+			# set up states with variables they need
+			child.enemy = enemy
+			child.playback = animation_tree["parameters/playback"]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if(current_state.next_state != null):
+		switch_states(current_state.next_state)
+		
+		current_state.state_process(_delta)
+		# checks for next_state and calls the cleanup function
+		
+		
+# Accessors for state variables
+func check_if_can_move():
+	return current_state.can_move
+func check_speed():
+	return current_state.speed
+
+# Functions
+func switch_states(new_state : EnemyState):
+	if (current_state != null):
+		current_state.on_exit()
+		current_state.next_state = null
+		
+	current_state = new_state
+	current_state.on_enter()
+	# calls all the state specific enter/exit functions and cleans up
