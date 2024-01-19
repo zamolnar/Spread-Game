cbed38a60f8ff1bae690510b7029a63c00983130
Joshua Greer <jjgreer@asu.edu>
2024-01-19 13:46:30 -0700

    fixed player animation call

--- scenesAndScripts/player.gd
+++ scenesAndScripts/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@export var speed = 400
+@export var speed = 200
 var move_direction : Vector2 = Vector2.ZERO
 
 	
 func _physics_process(delta):
 	if (velocity.x > 1 || velocity.x < -1):
-		animated_sprite_2d.animation = "run"
+		#animated_sprite_2d.animation = "run"
+		animated_sprite_2d.play("run")
 	else:
-		animated_sprite_2d.animation = "idle"
+		animated_sprite_2d.play("idle")
 	
 	move_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity.x = move_direction.x * speed;
@@ -17,7 +18,11 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	#to be modified with non placeholder frog
-	var isLeft = velocity.x < 0
-	animated_sprite_2d.flip_h = isLeft
+	
+	if(velocity.x < 0):
+		animated_sprite_2d.flip_h = true
+	if (velocity.x > 0):
+		animated_sprite_2d.flip_h = false	
+	
 	
 	
